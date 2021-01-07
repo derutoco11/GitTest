3b5e559cc0f25badd17b08132204c7123f0fe645
derutoco11 <yugreenid@gmail.com>
2021-01-08 01:32:23 +0900

    make views
    create template file.

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const http = require('http');
 const express = require('express');
+const path = require('path');
 
 var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 app.get("/", function (req, res, next) {
   return res.send('Hello, World.');
