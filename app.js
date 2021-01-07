5da4cbf03197cda474c99ca7447b87d1c41320e8
derutoco11 <yugreenid@gmail.com>
2021-01-08 01:13:44 +0900

    set routing
    ルーティングを設定、テンプレートエンジンをpackageに追記

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const express = require('express');
 
 var app = express();
 
-app.use(function (req, res, next) {
+app.get("/", function (req, res, next) {
   return res.send('Hello, World.');
+});
+
+app.get("/hoge", function (req, res, next) {
+  return res.send('Hello, hoge.');
 });
 
 const server = http.createServer(app);
