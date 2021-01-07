411628a13fac9f2f4e16ba844e1b599093dd34a2
derutoco11 <yugreenid@gmail.com>
2021-01-08 02:19:37 +0900

    update schema and sendforms
    更新しました

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
 const http = require('http');
 const express = require('express');
 const path = require('path');
+const bodyparser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Players = require('./schema/Players');
 
 var app = express();
+mongoose.connect('mongodb://localhost:27017/jpnt', function (err) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('connected to mongo.');
+  }
+});
+
+app.use(bodyparser());
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.get("/", function (req, res, next) {
-  return res.send('Hello, World.');
+  return res.render('index', { title: 'Hello, World.' });
 });
 
-app.get("/hoge", function (req, res, next) {
-  return res.send('Hello, hoge.');
+app.get("/players", function (req, res, next) {
+  return res.render('players');
+});
+
+app.post("/players", function (req, res, next) {
+  var newPlayer = new Players({
+    name: req.body.name,
+    age: req.body.age
+  });
+  newPlayer.save((err) => {
+    if (err) throw err;
+    return res.redirect("/");
+  });
 });
 
 const server = http.createServer(app);
