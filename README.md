c22efa86e9eb3607fd2c3ea858a24b207772d68d
derutoco11 <yugreenid@gmail.com>
2021-01-11 00:19:29 +0900

    ignored
    test

--- README.md
+++
@@ -1,5 +0,0 @@
-# GitTest
-Test for win Git, Local is Git_Reps.
-
-Remote Name is GitTest.
-This Project - For my Practice Use.
--- schema/Players.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-const { stringify } = require('querystring');
-
-var Players = mongoose.Schema({
-  name: String,
-  age: Number
-});
-
-module.exports = mongoose.model('Players', Players)
